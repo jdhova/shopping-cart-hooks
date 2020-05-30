eb6138366f7dcf3925bb04d8dc8b8c80e1300cf8
Jude Okagu <juud0010@gmail.com>
2020-05-30 13:31:35 +0200

    styling nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Nav BAR HERE</div>;
+  return (
+    <div className='main-nav'>
+      <div className='nav-1'>
+        <h2>
+          Home
+          <a href='/' alt='home' />
+        </h2>
+      </div>
+      <div className='nav-2'>
+        <h3>
+          About <a href='/about' alt='about' />
+        </h3>
+
+        <h3>
+          Cart <a href='/about' alt='about' />
+          <i class='fas fa-shopping-cart'></i>
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

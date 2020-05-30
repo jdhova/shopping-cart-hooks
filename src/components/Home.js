accff5a62a21a438cb5795672cee68f234ae684a
Jude Okagu <juud0010@gmail.com>
2020-05-31 01:23:13 +0200

    hooks setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,22 +9,31 @@ const Home = () => {
     <div className='main-image'>
       <div className='shirtimg'>
         <img src={blackshirt} alt='Black shirt' />
+
         <h3>Black shirt</h3>
+        <h3>$24.30</h3>
+        <a href='#'>Add to Cart</a>
       </div>
 
       <div className='shirtimg'>
         <img src={blueshirt} alt='Black shirt' />
         <h3>Blue shirt</h3>
+        <h3>$22.20</h3>
+        <a href='#'>Add to Cart</a>
       </div>
 
       <div className='shirtimg'>
         <img src={redshirt} alt='Black shirt' />
         <h3>Red shirt</h3>
+        <h3>$20.00</h3>
+        <a href='#'>Add to Cart</a>
       </div>
 
       <div className='shirtimg'>
         <img src={whiteshirt} alt='Black shirt' />
         <h3>White shirt</h3>
+        <h3>$32.00</h3>
+        <a href='#'>Add to Cart</a>
       </div>
     </div>
   );

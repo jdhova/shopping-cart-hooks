1ea116c53e793e3dc6cfbc2ca4199dd52de339cc
Jude Okagu <juud0010@gmail.com>
2020-05-31 02:33:19 +0200

    context

---
+++ src/context/cart/cartState.js
@@ -0,0 +1,7 @@
+import React, { useReducer } from 'react';
+import cartContext from './cartContext';
+import cartReducer from './cartReducer';
+
+const addToBasket = () => {
+  console.log(' adding to basket');
+};
